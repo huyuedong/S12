b602481e105a8cd10f4860eccbacd1d4a19bbac3
Q1mi <liwenzhou7@gmail.com>
2016-01-11 17:02:53 +0800

    take exercises about the builtin functions in 'str' class

--- day2/str_function_ex.py
+++ day2/str_function_ex.py
@@ -7,17 +7,51 @@ str类的内建方法练习
 
 """
 name1 = 'alex'
-name2 = str('ERIC')    # 调用字符串类的__init__()方法
+name2 = str('ERIC')  # 调用字符串类的__init__()方法
 print(type(name1))
-print(dir(name1))    # 打印出类的成员
+print(dir(name1))  # 打印出类的成员
 
 # 包含
-result = name1.__contains__('er')
+result = name1.__contains__('ex')
 print(result)
-result = 'er' in name1
+result = 'ex' in name1
 print(result)
 
-#
+# 等于
+print(name1.__eq__(name2))
+
+# 字符串按照format_spec格式化
+"""
+ format_spec ::=  [[fill]align][sign][#][0][width][,][.precision][type]
+
+fill        ::=  <any character>
+
+align       ::=  "<" | ">" | "=" | "^"
+
+sign        ::=  "+" | "-" | " "
+
+width       ::=  integer
+
+precision   ::=  integer
+
+type        ::=  "b" | "c" | "d" | "e" | "E" | "f" | "F" | "g" | "G" | "n" | "o" | "s" | "x" | "X" | "%"
+
+fill是表示可以填写任何字符。
+
+align是对齐方式，<是左对齐， >是右对齐，^是居中对齐。
+
+sign是符号， +表示正号， -表示负号。
+
+width是数字宽度，表示总共输出多少位数字。
+
+precision是小数保留位数。
+
+type是输出数字值是的表示方式，比如b是二进制表示；比如E是指数表示；比如X是十六进制表示。
+"""
+# 共20个字符，name1右对齐
+print(name1.__format__('>20'))
+
+# 返回小写
 result = name2.casefold()
 print(result)
 
@@ -26,22 +60,103 @@ print(result)
 result = name1.center(20, '*')
 print(result)
 
-# count
+# 获取指定标签属性的值
+print(name1.__getattribute__('index'))
+
+# x.__getitem__(y) <==> x[y](返回str[key])
+print(name1.__getitem__(1))
+
+# 大于等于
+print(name1.__ge__(name2))
+
+# 大于
+print(name1.__gt__(name2))
+
+# 哈希
+print(name1.__hash__())
+
+# 返回一个字符串的迭代
+for i in name1.__iter__():
+	print(i)
+
+# 返回字符串的长度
+print(name1.__len__())
+
+# 首字母大写
+print(name1.capitalize())
+
+# 返回str的小写
+print(name2.casefold())
+
+# 居中 下例为：20个字符的宽度以'*'号填充，name1居中
+print(name1.center(20, '*'))
+
+# count 计数
 str1 = 'adasafafdsfqadasddfa'
 result = str1.count('a')
 print(result)
 
-# encode
+# encode 编码
 result = name1.encode('gbk')
 print(result)
 
-# endswith
+# endswith 以'xxx'结束，后面可跟索引
 result = str1.endswith('a')
 print(result)
 result = str1.endswith('s', 2, 4)
 print(result)
 
+# 替换tabs tabsize默认为8
+name_temp = 'a\nlex'
+print(name_temp.expandtabs())
+print(len(name_temp.expandtabs()))
+
+# 查找 返回索引值
+print(name1.find('e'))
+
+# 格式化
+str_temp = '{} is humor!'
+print(str_temp.format(name2))
+
+# 按映射格式化
+# format_map(self, mapping)
+
+# 返回索引值 index(self, sub, start=None, end=None) 没有返回0
+print(name1.index('l'))
+
+str_temp = 'a1b2c3'
+# 判断字符串是否是字母或数字
+print(str_temp.isalnum())
+
+# 判断字符串是否只是字母
+print(str_temp.isalpha())
+
+# 判断是否只包含十进制字符
+str_temp = '0.3a0.4b'
+str_temp1 = '0.3'
+print(str_temp.isdecimal())
+print(str_temp1.isdecimal())
+
+# 判断字符串中是否只有数字
+str_temp = '12345'
+print(str_temp.isdigit())
+
+# 判断是否是标识符
+str_temp = 'class'
+print(str_temp.isidentifier())
+
+# 判断字符串中是否都是小写
+str_temp = 'eric'
+str_temp1 = 'Eric'
+print(str_temp.islower())
+print(str_temp1.islower())
+
+# 判断字符串中是否都是数值型字符串
+
+
 # 所有首字母大写
-str2 = 'alex is sb!'
-result = str2.title()
+str_temp = 'eric is humor!'
+result = str_temp.title()
 print(result)
+
+
