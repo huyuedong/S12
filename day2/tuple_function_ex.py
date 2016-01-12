0110a7663276397eb1385c180b64dfd06e47e458
Q1mi <liwenzhou7@gmail.com>
2016-01-12 10:45:14 +0800

    builtin functions in class 'tuple'

--- day2/tuple_function_ex.py
+++ day2/tuple_function_ex.py
@@ -2,4 +2,22 @@
 # -*- coding: utf-8 -*-
 # __author__ = "Q1mi"
 
-# index
+"""
+tuple的内建方法
+* -元祖的元素不可变
+* -元祖的元素的元素可以变
+"""
+
+# 计数
+t1 = (1, 1, 2, 3, 4, 4, 1)
+print(t1.count(1))
+
+# 索引
+print(t1.index(2))
+
+print('line:18'.center(20, '='))
+
+t1 = (1, 2, {'k1': 'v1'})
+t1[2]['k1'] = 2
+print(t1)
+print(t1[2])
