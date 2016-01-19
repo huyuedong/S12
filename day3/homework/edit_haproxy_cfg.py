e5726f56329df1185251a2fda0c456aff8c606e6
Q1mi <liwenzhou7@gmail.com>
2016-01-19 16:31:59 +0800

    complete the check function

--- day3/homework/edit_haproxy_cfg.py
+++ day3/homework/edit_haproxy_cfg.py
@@ -5,23 +5,7 @@
 """
 通过python程序可以对ha配置文件进行增删改，不再是以往的打开文件进行直接操作。
 """
-import json
-line_list = []
-line2_list = []
-
-
-# 初始化配置文件
-def init_cfg():
-	with open('haproxy.cfg', 'r') as f:
-		for line in f.readlines():
-			line_info = line.rstrip()
-			if line_info.startswith(' '):
-				line2_list.append(line_info.strip())
-			else:
-				line_list.append(line_info.strip())
-	return line_list, line2_list
-n = init_cfg()
-print(n)
+import string
 
 
 # 获取用户输入
@@ -55,18 +39,47 @@ def get_user_input():
 			print("无效的输入，请重新输入！")
 
 
-# 打印分项菜单
-def print_info(tag):
+# 获取用户输入的供查询的url
+def get_url_info():
 	while True:
-		if tag == 1:
-			read = input("请输入backend:").strip()
-			read = json.loads(read)
-			return read
-		elif tag == 2:
-			read = input("请输入要新加的记录：").strip()
-			read = json.loads(read)
-			return read
-		elif tag == 3:
-			read = input("请输入要删除的记录：").strip()
-			read = json.loads(read)
-			return read
+		url = input("Please input the url you want to check:").strip()
+		if len(url.split('.')) == 3:
+			if url.split('.')[2] in string.ascii_letters:
+				return url
+		else:
+			print("invalid url，please try again!")
+
+
+# 查找
+def show_info(url):
+	server_list = []
+	with open('haproxy.cfg') as f:
+		flag = False
+		for i in f.readlines():
+			# 匹配到用户输入的url时,开始统计server信息
+			if i.rstrip() == 'backend {}'.format(url):
+				flag = True
+				continue
+			# 匹配到下一条backend时就退出
+			elif i.startswith('backend'):
+				break
+			# 将符合条件的server信息录入列表
+			elif all([i.startswith(' '), i, flag]):
+				server_list.append(i.strip())
+	if len(server_list) == 0:
+		print("Sorry,there is no records about your input!")
+	else:
+		for i in server_list:
+			print(i)
+
+
+# 主函数
+def main():
+	num = get_user_input()
+	if num == 1:
+		ur = get_url_info()
+		show_info(ur)
+
+
+if __name__ == '__main__':
+	main()
