0d4165a6e5c5d067e14f8898965058c343f9a17b
Q1mi <liwenzhou7@gmail.com>
2016-01-26 17:01:39 +0800

    add files about homework of day 4

--- day4/homework/calculator_v0.1.py
+++ day4/homework/calculator_v0.1.py
@@ -10,17 +10,58 @@
 """
 import re
 
+
+# 运算
+def calc_func(arg):
+	if "*" in arg:
+		l_demo = arg.split("*")
+		return float(l_demo[0]) * float(l_demo[1])
+	elif "/" in arg:
+		l_demo = arg.split("/")
+		return float(l_demo[0]) / float(l_demo[1])
+	elif "+" in arg:
+		l_demo = arg.split("+")
+		return float(l_demo[0]) + float(l_demo[1])
+	elif "-" in arg:
+		l_demo = arg.split("-")
+		return float(l_demo[0]) - float(l_demo[1])
+
+
+# 找括号
+def find_brackets(arg):
+	tmp = re.search((r'\([^\(\)]+\)]', arg))
+	if tmp:
+		return tmp.group()
+	else:
+		return None
+
 s = "1 - 2 * ( (60-30 +(-40/5) * (9-2*5/3 + 7 /3*99/4*2998 +10 * 568/14 )) - (-4*3)/ (16-3*2) )"
+# 先去除掉空格
+s = re.sub(r'\s+', '', s)
+print(s)
+
 # 找到最里面的括号
 result = re.search(r'\([^\(\)]+\)', s)
 if result:
 	print(result.group())
-	# 去除运算符和括号，得到数字的列表
-	n = re.split(r'[\+\-\*\/]', result.group().strip('\(\)'))
-	print(n)
-	# 去除数字和括号，得到运算符的列表
-	m = re.split(r'\d+', result.group().strip('\(\)'))
-	print(m)
-# 将字符串的前面和后面放进列表，把匹配到的字符串算出数值之后再传进去
-result2 = s.split(result.group())
+
+# 将匹配到的结果去除括号
+result = result.group().strip("()")
+
+result2 = calc_func(result)
 print(result2)
+result2 = str(result2)
+
+# 将字符串的前面和后面放进列表，把匹配到的字符串算出数值之后再传进去
+result3 = s.split(result)
+result5 = result2.join(result3)
+print(result5)
+print("{0}{1}{2}".format(result3[0], result2, result3[1]))
+
+# result = find_brackets(result3)
+# if result:
+# 	result = result.strip("()")
+# 	result4 = calc_func(result)
+# 	print(result4)
+# else:
+# 	print("ERROR!")
--- day4/homework/test.py
+++ day4/homework/test.py
@@ -4,7 +4,13 @@
 
 import re
 
-s = "1+3*(6/2)+4"
+s = "1+3*(-60/2)+4"
 result = re.search(r'\(.+\)', s)
 if result:
 	print(result.group())
+	m = result.group().strip("()")
+
+	if "/" in m:
+		l = m.split("/")
+		print(int(l[0]) / int(l[1]))
+		print(float(l[0]) / float(l[1]))
