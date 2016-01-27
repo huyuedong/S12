b5ee79b93e94a9c87b5d91dc8c92efbb12f2e1e9
Q1mi <liwenzhou7@gmail.com>
2016-01-27 22:01:17 +0800

    add version 0.2

--- day4/homework/test_2.py
+++ day4/homework/test_2.py
@@ -3,8 +3,8 @@
 # __author__ = "Q1mi"
 
 import re
-s = "9-3.3333333333333335+173134.50000000003+405.7142857142857"
-result = re.search(r'[\-\+]?\d*\.?\d+[\+\-]\d+\.?\d*', s)
+s = "*3+*(*"
+result = re.search(r'(\(\*)(\/0)', s)
 if result:
 	print(result.group())
 
