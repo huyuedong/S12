028fbb7ad7ea1b764400ef82fcbb953765e90928
Q1mi <liwenzhou7@gmail.com>
2016-02-16 17:04:06 +0800

    add files about the homework of day 5.

--- day5/homework/atm_mall/credit_card/admin_menu.py
+++ day5/homework/atm_mall/credit_card/admin_menu.py
@@ -28,21 +28,19 @@ def change_limit(db):
 					if option2 == "1":
 						db[card_number]["limit"] = int(option)
 						print("卡号{}的额度修改为{}。".format(card_number, db[card_number]["limit"]))
-						break
+						return db
 					elif option2.upper() == "B":
 						break
 					elif option2.uooer() == "Q":
-						loop_flag = False
-						break
+						return db
 				elif option.upper() == "B":
 					break
 				elif option.upper() == "Q":
-					loop_flag = "Q"
-					break
+					return db
 				else:
 					print("无效的输入，请重新输入！")
 		elif card_number.upper() == "Q":
-			break
+			return db
 		else:
 			print("卡号不存在请重新输入！")
 
@@ -54,30 +52,86 @@ def add_card_account(db):
 		db_demo = {}
 		card_id = format(len(db)+1, "8=8")  # 生成一个8位的卡号
 		db_demo[card_id] = {}
-		card_name = input("请输入卡主姓名：").strip()
+		card_name = input("请输入卡主姓名,Q退出：").strip()
 		card_limit = input("请输入卡片额度：").strip()
-		bool_a = re.match(r'^[a-zA-Z.\u4e00-\u9fa5]{1,26}$', card_name)
-		bool_b = re.match(r'^\d{1,8}$', card_limit)
-		if all([bool_a, bool_b]):
+		bool_a = re.match(r'^[a-zA-Z.]{4,26}$', card_name)  # 英文名字4-26位
+		bool_b = re.match(r'^[\u4e00-\u9fa5]{2,4}$', card_name)    # 中文名字2-4位
+		bool_c = re.match(r'^\d{1,8}$', card_limit)
+		if all([(bool_a or bool_b), bool_c]):
+			# 初始化用户信息
 			db_demo[card_id]["name"] = card_name
 			db_demo[card_id]["limit"] = int(card_limit)
 			db_demo[card_id]["current_limit"] = int(card_limit)
-			db_demo[card_id]["cash_limit"] = db_demo[card_id]["current_limit"] * 0.5
+			db_demo[card_id]["cash_limit"] = int(db_demo[card_id]["current_limit"] * 0.5)
 			db_demo[card_id]["lock_flag"] = 0
 			db_demo[card_id]["password"] = "b8b28fcfe009057f2ef7362b1e91fe7a"
 			db_demo[card_id]["bill"] = 0
 			db_demo[card_id]["retry_count"] = 0
 			db_demo[card_id]["created_date"] = datetime.date.today()
-			option = input("确认添加按1，Q退出：")
-			if option.upper() == "1":
-				print("确认添加！")
-				db.update(db_demo)
-				print("添加完成！")
-				break
-			elif option.upper() == "Q":
-				break
-			else:
-				print("无效的输入，请重新输入！")
+			while True:
+				option = input("确认添加按1，B返回Q退出：")
+				if option == "1":
+					print("正在添加。。。")
+					db.update(db_demo)  # 更新数据
+					print("添加完成！")
+					return db
+				elif option.upper() == "B":
+					break
+				elif option.upper() == "Q":
+					return db
+				else:
+					print("无效的输入，请重新输入！")
+		elif card_name.upper() == "Q":
+			return db
+		else:
+			print("无效的输入，请重新输入！")
+
+
+# 冻结账户
+def frozen_account(db):
+	print("这是冻结账户菜单。。。")
+	loop_flag = True
+	while loop_flag:
+		card_number = input("请输入要冻结的卡号（Q退出）：").strip()
+		if db.get(card_number):
+			print("卡号：{}，姓名：{}，当前账单：{}".format(card_number, db[card_number]["name"], db[card_number]["bill"]))
+			while loop_flag:
+				option = input("确认冻结按1，B返回，Q退出：").strip()
+				if option == "1":
+					db[card_number]["lock_flag"] = 1
+					return db
+				elif option.upper() == "B":
+					break
+				elif option.upper() == "Q":
+					return db
+				else:
+					print("无效的输入，请重新输入！")
+		elif card_number.upper() == "Q":
+			return db
+		else:
+			print("卡号不存在，请重新输入！")
+
+
+# 重置密码
+def reset_password(db):
+	print("这是重置密码菜单。。。")
+	while True:
+		card_number = input("请输入要重置密码的卡号,Q退出：").strip()
+		if card_number.upper() == "Q":
+			return db
+		elif all([card_number.isdigit(), db.get(card_number)]):
+			while True:
+				option = input("您要重置密码的卡号为{},用户名为{}，1确认，B返回，Q退出！".format(card_number, db[card_number]["name"]))
+				if option == "1":
+					db[card_number]["password"] = "b8b28fcfe009057f2ef7362b1e91fe7a"
+					print("卡号：{}的密码已重置为初始密码！".format(card_number))
+					return db
+				elif option.upper() == "B":
+					break
+				elif option.upper() == "Q":
+					return db
+				else:
+					print("无效的输入，请重新输入！")
 		else:
 			print("无效的输入，请重新输入！")
 
@@ -90,4 +144,10 @@ info = {
 			"cash_limit": 5000, "current_limit": 10000, "bill": 0, "retry_count": 0, "created_date": datetime.date(2016, 2, 2)},
 }
 
-add_card_account(info)
+a = change_limit(info)
+# a = add_card_account(info)
+# a = frozen_account(info)
+# a = reset_password(info)
+
+for i in a:
+	print(a[i])
--- day5/homework/atm_mall/credit_card/user_menu.py
+++ day5/homework/atm_mall/credit_card/user_menu.py
@@ -10,3 +10,111 @@
 -提现
 -修改密码
 """
+
+import datetime
+
+
+# 查询菜单
+def check_info(card_id, db):
+	print("这是查询额度、账单菜单。。。")
+	while True:
+		option = input("1查询额度，2查询账单，Q退出！：").strip()
+		if option == "1":
+			print("查询额度。。。")
+			print("您总额度为：{}，当前额度：{}，取现额度：{}".format(db[card_id]["limit"], db[card_id]["current_limit"], db[card_id]["cash_limit"]))
+		elif option == "2":
+			print("查询账单。。。")
+			print("您当前的账单为：{}".format(db[card_id]["bill"]))
+		elif option.upper() == "Q":
+			break
+		else:
+			print("无效的输入，请重新输入！")
+
+
+# 还款
+def repay_bill(card_id, db):
+	print("这是还款菜单。。。")
+	loop_flag = True
+	while loop_flag:
+		option = input("1：自己还款，2：替别人还款，3：退出").strip()
+		if option == "1":
+			while loop_flag:
+				bill = db[card_id]["bill"]
+				print("您当前账单为：{}".format(bill))
+				option2 = input("请输入您要还款的金额：").strip()
+				if option2.isdigit():
+					if int(option2) > int(bill):
+						while loop_flag:
+							print("多余的钱也没有利息哦~")
+							# 用临时变量存储
+							bill_temp = 0
+							current_limit_demo = int(option2) - int(bill)
+							option3 = input("你账单：{}，本次还款：{}，1确认，B返回，Q退出：".format(bill, option2))
+							if option3 == "1":
+								db[card_id]["bill"] = bill_temp
+								db[card_id]["current_limit"] = current_limit_demo
+								return db
+							elif option3.upper() == "B":
+								break
+							elif option3.upper() == "Q":
+								return db
+							else:
+								print("无效的输入，请重新输入！")
+					elif int(option2) == 0:
+						print("别逗了，你丫根本没放钱进来。。。")
+					else:
+						while loop_flag:
+							bill_temp = bill - int(option2)
+							print("你本次还款：{}元，剩余账单：{}元".format(option2, bill_temp))
+							option4 = input("1确认还款，B返回，Q退出：")
+							if option4 == "1":
+								db[card_id]["bill"] = bill_temp
+								return db
+							elif option4.upper() == "B":
+								break
+							elif option4.upper() == "Q":
+								return db
+							else:
+								print("无效的输入，请重新输入！")
+				elif option2.upper() == "B":
+					break
+				elif option2.upper() == "Q":
+					return db
+				else:
+					print("无效的输入，请重新输入！")
+		elif option == "2":
+			pass
+		elif option.upper() == "Q":
+			return db
+		else:
+			print("无效的输入，请重新输入！")
+
+
+# 转账
+def transfer_accounts(db):
+	print("这是转账菜单。。。")
+
+
+# 提现
+def enchashment(db):
+	print("这是取现的菜单。。。")
+
+
+# 修改密码
+def change_password():
+	print("这是修改密码的菜单。。。")
+
+
+info = {
+	"88888881":
+		{"name": "alex", "lock_flag": 0, "password": "b8b28fcfe009057f2ef7362b1e91fe7a", "limit": 20000,
+			"cash_limit": 10000, "current_limit": 20000, "bill": 8888, "retry_count": 0, "created_date": datetime.date(2016, 2, 1)},
+	"88888882":
+		{"name": "john", "lock_flag": 0, "password": "b8b28fcfe009057f2ef7362b1e91fe7a", "limit": 10000,
+			"cash_limit": 5000, "current_limit": 10000, "bill": 0, "retry_count": 0, "created_date": datetime.date(2016, 2, 2)},
+}
+
+# check_info("88888881", info)
+a = repay_bill("88888881", info)
+for i in a:
+	print(a[i])
