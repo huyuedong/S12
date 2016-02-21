6e49e89963a89845e8aa52d6db16b6bf5132f8dc
Q1mi <liwenzhou7@gmail.com>
2016-02-21 20:52:39 +0800

    add files of day6

--- day5/logging_ex.py
+++ day5/logging_ex.py
@@ -6,15 +6,36 @@
 logging模块的练习
 """
 import logging
-import time
-import datetime
-import os
-logfile = "D:\GitHub\S12\day5\example.log"
 
-logging.warning("user [alex] attempted wrong passwd more than 3 times")
-logging.critical("server is down!")
+# logfile = "example.log"
+# logging.basicConfig(filename=logfile, level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+#
+# logging.warning("user [alex] attempted wrong passwd more than 3 times")
+# logging.critical("server is down!")
+# logging.debug('This message should be go to the log file...')
+# logging.warning("Server is down!")
 
-logging.basicConfig(filename=logfile, level=logging.INFO)
-logging.debug('This message should be go to the log file...')
-logging.info("{} {}".format(datetime.datetime.today(), time.ctime(time.time())))
-logging.warning("Server is down!")
+# 同时输出到文件和屏幕
+logger = logging.getLogger("LOG-TEST")  # 创建一个logger
+logger.setLevel(logging.DEBUG)  # 设定logging 级别
+
+ch = logging.StreamHandler()    # 创建一个输出到屏幕的handler
+ch.setLevel(logging.INFO)   # 设置logging级别
+
+fh = logging.FileHandler("test.log")  # 创建一个文件handler
+fh.setLevel(logging.WARNING)    # 设置logging级别
+
+# 创建格式
+formatter = logging.Formatter('%(asctime)s - %(filename)s - %(name)s - %(levelname)s - %(message)s')
+
+ch.setFormatter(formatter)
+fh.setFormatter(formatter)
+
+logger.addHandler(ch)
+logger.addHandler(fh)
+
+logger.debug('debug message')
+logger.info('info message')
+logger.warn('warn message')
+logger.error('error message')
+
--- day5/shelve_ex.py
+++ day5/shelve_ex.py
@@ -4,31 +4,32 @@
 
 """
 shelve模块
+与pickle的不同是：pickle dump多次，然后按顺序load多次。
+shelve打开一次就可以按照key来读取
 """
 
 import shelve
-import pickle
 
-# d = shelve.open("test3")
-#
-#
-# # 定义一个测试类
-# class TestDemo(object):
-# 	def __init__(self, n):
-# 		self.n = n
-#
-# t = TestDemo(123)
-#
-# name = ["alex", "john", "eric"]
-#
-# d["test1"] = name   # 持久化列表
-# d["test2"] = {"a": 1, "b": 2, "c": 3}   # 持久化列表
-# d["test3"] = t  # 持久化类实例
-#
-# d.close()
+
+# 定义一个测试类
+class TestDemo(object):
+	def __init__(self, n):
+		self.n = n
+
+t = TestDemo(123)
+
+name = ["alex", "john", "eric"]
+
+d = shelve.open("test3")
+
+d["test1"] = name   # 持久化列表
+d["test2"] = {"a": 1, "b": 2, "c": 3}   # 持久化列表
+d["test3"] = t  # 持久化类实例
+
+d.close()
 
 p = shelve.open("test3")
-for i in p.keys():
-	print(i)
-	print(p[i])
+print(p["test1"])
+temp = p.get("test3")
+print(temp.n)
 p.close()
