b230e1d327055833ec4b51cb8eed671f191d0d99
Q1mi <liwenzhou7@gmail.com>
2016-02-29 22:33:30 +0800

    add files about the homework of day 6.

--- day7/sock_client3.py
+++ day7/sock_client3.py
@@ -8,15 +8,33 @@ socket 客户端
 
 import socket
 
-ip_port = ('127.0.0.1', 4444)
+ip_port = ('127.0.0.1', 5444)
 sk = socket.socket()
 sk.connect(ip_port)
 
 while True:
-	send_data = input("cmd:").strip()
-	send_data = bytes(send_data, 'utf8')
-	sk.send(send_data)
-	recv_data = sk.recv(1024)
-	print(type(recv_data))
-	print(str(recv_data.decode()))
+        send_data = input("cmd:").strip()
+        if len(send_data) == 0:continue
+        elif send_data == "q":break
+        send_data = bytes(send_data, 'utf8')
+        sk.send(send_data)
+        cmd_res_msg = sk.recv(100)
+        cmd_res_msg = str(cmd_res_msg.decode()).split("|")
+        print("server response:{}".format(cmd_res_msg))
+        if cmd_res_msg[0] == "CMD_RESULT_SIZE":
+            cmd_res_size = int(cmd_res_msg[1])
+            sk.send(b"CLIENT_READY_TO_RECV")
+            # 定义一个空的字符串用于拼接接收到的数据
+            res = ""
+            # 已经接收的数据量
+            received_size = 0
+            # 当接受的数据量小于之前传过来的数据量的值时，一直接收数据
+            while received_size < cmd_res_size:
+                data = sk.recv(500)
+                received_size += len(data)
+                res += str(data.decode())
+            else:
+                print(str(res))
+                print("==============recv done============")
 sk.close()
+
--- day7/sock_server3.py
+++ day7/sock_server3.py
@@ -12,27 +12,30 @@
 import socket
 import subprocess
 
-ip_port = ('127.0.0.1', 4444)
+ip_port = ('127.0.0.1', 5444)
 sk = socket.socket()
 sk.bind(ip_port)
 sk.listen(5)
 
 while True:
-	print("server waiting...")
-	conn, addr = sk.accept()
-	while True:
-		try:
-			client_data = conn.recv(1024)
-			if not client_data: continue
-			str_client_data = str(client_data, 'utf8').strip()
-			print("the cmd:{}".format(str_client_data))
-			# 管道执行命令
-			result = subprocess.Popen(str_client_data, shell=True, stdout=subprocess.PIPE)
-			# 获取命令显示的结果
-			result_data = result.stdout.read()
-			print(type(result_data))
-		except Exception:
-			print("collection closed, break")
-			break
-		conn.send(result_data)
-	sk.close()
+        print("server waiting...")
+        conn, addr = sk.accept()
+        while True:
+                client_data = conn.recv(1024)
+                if not client_data:break
+                str_client_data = str(client_data, 'utf8').strip()
+                print("the cmd:{}".format(str_client_data))
+                # 管道执行命令
+                result = subprocess.Popen(str_client_data, shell=True, stdout=subprocess.PIPE)
+                # 获取命令显示的结果
+                result_data = result.stdout.read()
+                if len(result_data) == 0:
+                    result_data = b'cmd execution has no output...'
+                ack_msg = bytes("CMD_RESULT_SIZE|{}".format(len(result_data)), 'utf8')
+                # 先发送数据大小
+                conn.send(ack_msg)
+                client_ack = conn.recv(50)
+                if client_ack.decode() == "CLIENT_READY_TO_RECV":
+                    conn.send(result_data)
+        sk.close()
+
