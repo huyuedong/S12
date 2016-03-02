b1162070e0288e1616354719b77c6070d5b77366
Q1mi <liwenzhou7@gmail.com>
2016-03-02 21:52:00 +0800

    add files about the homework of day 7.

--- day7/homework/ftp_server.py
+++ day7/homework/ftp_server.py
@@ -9,30 +9,24 @@ FTP Server端
 import socket
 import subprocess
 
-ip_port = ('127.0.0.1', 5444)
-sk = socket.socket()
-sk.bind(ip_port)
-sk.listen(5)
 
-while True:
-        print("server waiting...")
-        conn, addr = sk.accept()
-        while True:
-                client_data = conn.recv(1024)
-                if not client_data:break
-                str_client_data = str(client_data, 'utf8').strip()
-                print("the cmd:{}".format(str_client_data))
-                # 管道执行命令
-                result = subprocess.Popen(str_client_data, shell=True, stdout=subprocess.PIPE)
-                # 获取命令显示的结果
-                result_data = result.stdout.read()
-                if len(result_data) == 0:
-                    result_data = b'cmd execution has no output...'
-                ack_msg = bytes("CMD_RESULT_SIZE|{}".format(len(result_data)), 'utf8')
-                # 先发送数据大小
-                conn.send(ack_msg)
-                client_ack = conn.recv(50)
-                if client_ack.decode() == "CLIENT_READY_TO_RECV":
-                    conn.send(result_data)
-        sk.close()
+class FTPServer(object):
 
+	def __init__(self, obj):
+		self.obj = obj
+
+	# 查看
+	def show(self):
+		pass
+
+	# 下载
+	def get(self):
+		pass
+
+	# 上传
+	def put(self):
+		pass
+
+	# 改变路径
+	def change_dir(self):
+		pass
