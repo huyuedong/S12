989b3f36e012398f441580b15eaf071e866fdf86
Q1mi <liwenzhou7@gmail.com>
2016-03-09 21:46:53 +0800

    add  files of day8

--- day8/paramiko_ex.py
+++ day8/paramiko_ex.py
@@ -16,7 +16,7 @@ ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 # 连接服务器
-ssh.connect(hostname="172.18.26.193", port=22, username="root", password="rootroot")
+ssh.connect(hostname="192.168.183.131", port=22, username="root", password="rootroot")
 
 # 执行命令
 stdin, stdout, stderr = ssh.exec_command("df")
