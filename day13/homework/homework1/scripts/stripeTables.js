1a8c0c5c1b9be6030c32ed344bc1491d6daa319c
Q1mi <liwenzhou7@gmail.com>
2016-04-16 14:08:54 +0800

    add files about homework1 of day13.

--- day13/homework/homework1/scripts/stripeTables.js
+++ day13/homework/homework1/scripts/stripeTables.js
@@ -1,47 +1,58 @@
 /**
  * Created by liwenzhou on 2016/4/13.
  */
-function stripeTables () {
-    if (!document.getElementsByTagName) return false;
-    var tables = document.getElementsByTagName("table");
-    var odd, rows;
-    for (var i=0;i<tables.length;i++) {
-        odd = false;
-        rows = tables[i].getElementsByTagName("tr");
-        if (rows.length < 1) return false;
-        for (var j=0;j<rows.length;j++) {
-            if (odd == true) {
-                rows[j].style.backgroundColor = "#ffffcc";
-                odd = false;
-            } else {
-                odd = true;
-            }
-        }
-    }
+//function stripeTables () {
+//    if (!document.getElementsByTagName) return false;
+//    var tables = document.getElementsByTagName("table");
+//    var odd, rows;
+//    for (var i=0;i<tables.length;i++) {
+//        odd = false;
+//        rows = tables[i].getElementsByTagName("tr");
+//        if (rows.length < 1) return false;
+//        for (var j=0;j<rows.length;j++) {
+//            if (odd == true) {
+//                rows[j].style.backgroundColor = "#EFF9A0";
+//                odd = false;
+//            } else {
+//                odd = true;
+//            }
+//        }
+//    }
+//}
+//为奇数行（第1,3,5行...）添加背景
+function stripeTables() {
+    $("tbody tr:nth-child(even)").addClass("tr_alt");
+
 }
 //鼠标悬停在表格的某一行时，该行文本加黑加粗
-function highlightRows () {
-    if (!document.getElementsByTagName) return false;
-    var rows = document.getElementsByTagName("tr");
-    for (var i=0;i<rows.length;i++) {
-        rows[i].onmouseover = function() {
-            this.style.fontWeight = "bold";
-        };
-        rows[i].onmouseout = function() {
-            this.style.fontWeight = "normal";
+//function highlightRows () {
+//    if (!document.getElementsByTagName) return false;
+//    var rows = document.getElementsByTagName("tr");
+//    for (var i=0;i<rows.length;i++) {
+//        rows[i].onmouseover = function() {
+//            this.style.fontWeight = "bold";
+//        };
+//        rows[i].onmouseout = function() {
+//            this.style.fontWeight = "normal";
+//        }
+//    }
+//}
+function highlightRows() {
+    $("tr").hover(
+        function() {
+            $(this).css("fontWeight", "bold");
+        },
+        function() {
+            $(this).css("fontWeight", "normal");
         }
-    }
+    );
 }
-//为元素节点追加class属性
-function addClass(element, value) {
-    if (!element.className) {
-        element.className = value
-    } else {
-        newClassName = element.className;
-        newClassName += " ";
-        newClassName += value;
-        element.className = newClassName;
-    }
-}
-addLoadEvent(stripeTables);
-addLoadEvent(highlightRows);
+
+//简写方法
+$(function() {
+   stripeTables();
+});
+//推荐方法
+$(document).ready(function() {
+   highlightRows();
+});
