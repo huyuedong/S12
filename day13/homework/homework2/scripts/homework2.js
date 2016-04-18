1b5ab038058877169874d39998671b3b70b44e12
Q1mi <liwenzhou7@gmail.com>
2016-04-18 15:45:07 +0800

    add homework files about homework1 of day13.

---
+++ day13/homework/homework2/scripts/homework2.js
@@ -0,0 +1,32 @@
+/**
+ * Created by liwenzhou on 2016/4/18.
+ */
+//为奇数行（第1,3,5行...）添加背景
+function stripeTables() {
+    $("tbody tr:nth-child(even)").addClass("tr_alt");
+
+}
+//鼠标悬停在表格的某一行时，该行文本加黑加粗
+function highlightRows() {
+    $("tr").hover(
+        function() {
+            $(this).css("fontWeight", "bold");
+        },
+        function() {
+            $(this).css("fontWeight", "normal");
+        }
+    );
+}
+function popEditPage() {
+    $("a").click(function(ths) {
+        $("#pop-page").removeClass("hidden");
+        $("#cancel-button").click(function() {
+            $("#pop-page").addClass("hidden");
+        })
+    })
+}
+$(function() {
+    stripeTables();
+    highlightRows();
+    popEditPage();
+});
