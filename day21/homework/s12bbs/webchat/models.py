a23f3d49b9c3468211eae4e1554768bf1be61a35
Q1mi <liwenzhou7@gmail.com>
2016-06-13 16:54:42 +0800

    add homework files of day21
    add homework files of day21

--- day21/homework/s12bbs/webchat/models.py
+++ day21/homework/s12bbs/webchat/models.py
@@ -10,8 +10,8 @@ class WebGroup(models.Model):
 	name = models.CharField(u"群名", max_length=64)
 	brief = models.CharField(u"群简介", max_length=255)
 	owner = models.ForeignKey(UserProfile, related_name="group_owner", verbose_name=u"拥有者")
-	admins = models.ManyToManyField(UserProfile, blank=True, null=True, related_name="group_admins", verbose_name=u"管理员")
-	members = models.ManyToManyField(UserProfile, blank=True, null=True, related_name="group_members", verbose_name=u"群成员")
+	admins = models.ManyToManyField(UserProfile, blank=True, related_name="group_admins", verbose_name=u"管理员")
+	members = models.ManyToManyField(UserProfile, blank=True, related_name="group_members", verbose_name=u"群成员")
 	max_members = models.IntegerField(u"最大成员数", default=200)
 
 	def __str__(self):
