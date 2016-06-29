a62fa2f44fbed1ca3ca1b40775d768c541e150d2
Q1mi <liwenzhou7@gmail.com>
2016-06-29 17:03:51 +0800

    update files in class
    update files in class

--- Stark/Arya/backends/utils.py
+++ Stark/Arya/backends/utils.py
@@ -3,7 +3,7 @@
 # __author__ = "Q1mi"
 # Email: master@liwenzhou.com
 
-
+from Stark import settings
 from Arya import action_list
 from Arya import models
 
@@ -32,10 +32,15 @@ class ArgvManagement(object):
 			self.help_msg()
 		module_name = self.argvs[1]
 		if "." in module_name:
-			module_name, mod_method = module_name.split(".")
-			module_instance = action_list.actions.get(module_name)
-			if module_instance:
-				module_instance(self.argvs, models)
-
+			module_name, module_method = module_name.split(".")
+			module_instance = action_list.actions.get(module_name)  # 获取匹配到的模块实例
+			if module_instance:  # 匹配到
+				module_obj = module_instance(self.argvs, models, settings)
+				module_obj.process()  # 提取主机
+				if hasattr(module_obj, module_method):
+					module_method_obj = getattr(module_obj, module_method)  # 解析
+					module_method_obj()  # 调用指定的指令
+				else:
+					exit("module:{} doesn't have method:{}.".format(module_name, module_method))
 		else:
 			exit("invalid module name argument.")
