f45e5b14ac6c6f522db248bf4b320093a04b225e
Q1mi <liwenzhou7@gmail.com>
2016-07-05 09:36:29 +0800

    add day24 files in class.
    add day24 files in class.

---
+++ day24/Stark/Arya/plugins/pkg.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author:Alex Li
+from Arya.backends.base_module import BaseSaltModule
+
+
+class Pkg(BaseSaltModule):
+
+
+    def is_required(self,*args,**kwargs):
+        return 'echo 0'
